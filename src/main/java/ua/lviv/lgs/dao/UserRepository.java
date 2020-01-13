0ff2ae429c4a6adabbe8ab49397fcf54c95522f5
LogosItAcademy <Tester99037@gmail.com>
2020-01-13 13:39:15 +0200

    added dao layer

---
+++ src/main/java/ua/lviv/lgs/dao/UserRepository.java
@@ -0,0 +1,10 @@
+package ua.lviv.lgs.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import ua.lviv.lgs.domain.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+	User findByEmail(String email);
+}
